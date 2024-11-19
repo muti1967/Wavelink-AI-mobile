7f4da1ac2b1d4a3ddc2eaad722ae5defa7dfd33f
muti1967 <mutishuman@gmail.com>
2024-11-19 11:57:21 -0800

    First Push through Xcode

---
+++ WaveLink_Mobile_AI/WaveLink_Mobile_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveLink_Mobile_AIApp.swift
+//  WaveLink_Mobile_AI
+//
+//  Created by Muti Shuman  on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WaveLink_Mobile_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaveLink_Mobile_AITests/WaveLink_Mobile_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  WaveLink_Mobile_AITests.swift
+//  WaveLink_Mobile_AITests
+//
+//  Created by Muti Shuman  on 11/19/24.
+//
+
+import Testing
+@testable import WaveLink_Mobile_AI
+
+struct WaveLink_Mobile_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
